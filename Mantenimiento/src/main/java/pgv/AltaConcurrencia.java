525c0c3badd1ad6584c3b62fa0159f5e9665fae5
Jorgell01 <yacolua@outlook.com>
2024-10-28 13:48:46 +0000

    Cambie alta concurrencia para hacerlo con un cachedpool

--- Mantenimiento/src/main/java/pgv/AltaConcurrencia.java
+++ Mantenimiento/src/main/java/pgv/AltaConcurrencia.java
@@ -4,17 +4,12 @@ import java.util.concurrent.*;
 
 public class AltaConcurrencia {
 
-    private final ThreadPoolExecutor executor;
+    private final ExecutorService executor;
 
     public AltaConcurrencia() {
-        // Configurar el ThreadPoolExecutor con un número mínimo y máximo de hilos
-        int corePoolSize = 10; // Número mínimo de hilos
-        int maximumPoolSize = 100; // Número máximo de hilos
-        long keepAliveTime = 60L; // Tiempo que los hilos inactivos esperarán antes de ser terminados
-        TimeUnit unit = TimeUnit.SECONDS;
-        BlockingQueue<Runnable> workQueue = new LinkedBlockingQueue<>();
 
-        executor = new ThreadPoolExecutor(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);
+        executor = Executors.newCachedThreadPool();
+
     }
 
     public void enviarTarea(Runnable task) {
@@ -36,11 +31,11 @@ public class AltaConcurrencia {
         AltaConcurrencia altaConcurrencia = new AltaConcurrencia();
 
         // Simulación de envío de tareas
-        for (int i = 0; i < 50; i++) {
+        for (int i = 0; i < 100; i++) {
             altaConcurrencia.enviarTarea(() -> {
                 System.out.println("Ejecutando tarea: " + Thread.currentThread().getName());
                 try {
-                    Thread.sleep(1000); // Simulación de trabajo
+                    Thread.sleep(2000); // Simulación de trabajo
                 } catch (InterruptedException e) {
                     Thread.currentThread().interrupt();
                 }
